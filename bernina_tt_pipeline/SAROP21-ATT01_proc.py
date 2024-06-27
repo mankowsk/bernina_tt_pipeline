d76a5fb6bdb0a0143017842569c1a017326d9190
“Roman <mankowsky_r@psi.ch>
2024-06-27 13:52:33 +0200

    finally working

--- bernina_tt_pipeline/SAROP21-ATT01_proc.py
+++ bernina_tt_pipeline/SAROP21-ATT01_proc.py
@@ -6,7 +6,7 @@ initialized = False
 
 def initialize(params):
     global initialized, buffer
-    buffer = deque(maxlen=params["buffer_length"])
+    buffer = deque(maxlen=params["dark_buffer_length"])
     initialized = True
 
 def normstep(step):
@@ -48,10 +48,12 @@ def get_max(c, dpx_poly=None, offset=0):
     mx = c[im]
 
     if dpx_poly:
-        poly = np.polyfit(np.arange(-dpx_poly//2,dpx_poly//2),c[im-dpx_poly//2:im+dpx_poly//2], 5)
-        root = np.roots(np.polyder(poly,1))
-        im+= np.real(root[np.argmin(np.abs(root))])
-        
+        try:
+            poly = np.polyfit(np.arange(-dpx_poly//2,dpx_poly//2),c[im-dpx_poly//2:im+dpx_poly//2], 5)
+            root = np.roots(np.polyder(poly,1))
+            im+= np.real(root[np.argmin(np.abs(root))])
+        except:
+            im=im
     return im + offset, mx
 
 def process(data, pid, timestanp, params):
@@ -59,7 +61,14 @@ def process(data, pid, timestanp, params):
         initialize(params)
     
     ## initialize all values sent to BS stream to None
-    edge_pos = xcorr_ampl = xcorr = arrival_time = ratio = tt_sig = dark = None
+    edge_pos = None
+    arrival_time = None
+    xcorr = None
+    xcorr_ampl = None
+    ratio = None
+    dark = None
+    tt_sig = None
+
     
     ## params used for evaluation    
     roi = params["roi"]
@@ -74,22 +83,22 @@ def process(data, pid, timestanp, params):
     is_laser_dark = params["is_laser_dark"]
     is_fel_dark = params["is_fel_dark"]
     if is_fel_dark:
-        is_fel_dark = events[[params["event_code_fel"]]]
+        is_fel_dark = events[params["event_code_fel"]]
     is_delayed = events[params["event_code_laser"]]
 
-    ## analyze
-    if (~is_delayed & ~is_fel_dark):
-        if len(buffer) > params["buffer_length"]-1:
+    # analyze
+    if (not is_delayed) & (not is_fel_dark):
+        if len(buffer) > params["dark_buffer_length"]-1:
             ref = get_reference_function(sigma_px=sigma_px, reflen=reflen, window=window) 
             edge_pos, xcorr_ampl, xcorr, ratio, dark = find_signal(tt_sig, ref, roi=roi, dpx_poly=dpx_poly)
             arrival_time = np.polyval(params["calibration"], edge_pos)*1e15
 
-    ## or add to dark reference
-    elif not (is_delayed & ~is_laser_dark):
+    # or add to dark reference
+    elif not (is_delayed) & (is_laser_dark):
         buffer.append(tt_sig)
 
     # To populate Chris panel
-    edge_results.update({
+    edge_results = {
         "SAROP21-ATT01:edge_pos": edge_pos,
         "SAROP21-ATT01:arrival_time": arrival_time,
         "SAROP21-ATT01:xcorr": xcorr,
@@ -98,7 +107,7 @@ def process(data, pid, timestanp, params):
         "SAROP21-ATT01:avg_dark_wf": dark,
         "SAROP21-ATT01:raw_wf": tt_sig,
         "SAROP21-ATT01:raw_wf_savgol": tt_sig,
-        })
+        }
     return edge_results
 
 
